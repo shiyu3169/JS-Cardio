c18aea1739c6c8aee659a7bc09e92df6a3fd4578
Shiyu Wang <shiyu3169@gmail.com>
2019-07-16 15:25:58 -0400

    change comment to be block

--- main.js
+++ main.js
@@ -1,80 +1,81 @@
-// Q1: Add all numbers
+/* // Q1: Add all numbers
 // Return a sum of all parameters entered regarless of the amount of numbers - NO ARRAYS
 // ex. addAll(2, 5, 6, 7) === 20
 
-// Solution - ES5 arguments & for loop
-// function addAll() {
-//   var args = Array.prototype.slice.call(arguments);
-//   var total = 0;
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
+Solution - ES5 arguments & for loop
+function addAll() {
+  var args = Array.prototype.slice.call(arguments);
+  var total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
 
-//   return total;
-// }
+  return total;
+}
 
-// Solution 2 - ES6 ...rest & reduce/ forEach
-// function addAll(...numbers) {
-//   return numbers.reduce((acc, cur) => acc + cur);
-// }
+Solution 2 - ES6 ...rest & reduce/ forEach
+function addAll(...numbers) {
+  return numbers.reduce((acc, cur) => acc + cur);
+}
 
-// console.log(addAll(2, 5, 6, 7));
+console.log(addAll(2, 5, 6, 7)); */
 
-// Q2: Sum All Primes
+/* // Q2: Sum All Primes
 // Pass in a number to loop up to and add all of the prime numbers.
 // A prime number is a whole number greater than 1 whose only factors are 1 and itself
-// ex. sumAllPrimes(10) == 17
-// function sumAllPrimes(num) {
-//   let total = 0;
+ex. sumAllPrimes(10) == 17
+function sumAllPrimes(num) {
+  let total = 0;
 
-//   function checkForPrime(i) {
-//     for (let j = 2; j < i; j++) {
-//       if (i % j === 0) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   }
+  function checkForPrime(i) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-//   for (let i = 2; i <= num; i++) {
-//     if (checkForPrime(i)) {
-//       total += i;
-//     }
-//   }
+  for (let i = 2; i <= num; i++) {
+    if (checkForPrime(i)) {
+      total += i;
+    }
+  }
 
-//   return total;
-// }
+  return total;
+}
 
-// console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(10)); */
 
-// Q3: Seek & Destroy
+/* // Q3: Seek & Destroy
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-// Solution - arguments, indexOf, filter
-// function seekAndDestroy(arr) {
-//   const args = Array.from(arguments);
+Solution - arguments, indexOf, filter
+function seekAndDestroy(arr) {
+  const args = Array.from(arguments);
 
-//   function filterArr(arr) {
-//     // Return true if Not in array
-//     return args.indexOf(arr) === -1;
-//   }
+  function filterArr(arr) {
+    // Return true if Not in array
+    return args.indexOf(arr) === -1;
+  }
 
-//   return arr.filter(filterArr);
-// }
+  return arr.filter(filterArr);
+}
 
-// Solution 2: ...rest, filter & includes
-// function seekAndDestroy(arr, ...rest) {
-//   return arr.filter(val => !rest.includes(val));
-// }
+Solution 2: ...rest, filter & includes
+function seekAndDestroy(arr, ...rest) {
+  return arr.filter(val => !rest.includes(val));
+}
 
-// console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6));
+console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6)); */
 
-// Q4: Sort By Height
-//  Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by thier heights in a non-descending order without moving the trees.
+/* // Q4: Sort By Height
+//  Some people are standing in a row in a park. There are trees between them which cannot be moved. 
+// Your task is to rearrange the people by thier heights in a non-descending order without moving the trees.
 // es.
-// a = [-1, 150, 190, 170, -1, -1, 160, 180]
-// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+a = [-1, 150, 190, 170, -1, -1, 160, 180]
+sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 function sortByHeight() {
   const arr1 = [];
   const arr2 = [];
@@ -96,4 +97,16 @@ function sortByHeight() {
 }
 
 const a = [-1, 150, 190, 170, -1, -1, 160, 180];
-console.log(sortByHeight(a));
+console.log(sortByHeight(a)); */
+
+// Q5 : Missing letters
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingletters("abcdefghijklmnopqrstuvwxyz") == undefined
+function missingleters(str) {
+  let compare = str.charCodeAt(str[0);
+}
+
+console.log(missingLetters('abce'));
